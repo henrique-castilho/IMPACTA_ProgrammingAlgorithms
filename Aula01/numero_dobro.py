76508c102c8a5df97fd46adc0803c89cd8dadf65
henrique-castilho <henrique.castilho.pires@gmail.com>
2026-02-24 20:32:45 -0300

    feat: adiciona exercícios de equações matemáticas

---
+++ Aula01/numero_dobro.py
@@ -0,0 +1,7 @@
+# Um número somado ao seu dobro resulta em 36. Qual é esse número?
+
+num = 36 / 3
+resultado = num + (num * 2)
+
+print(f"O número é {int(num)}")
+print(f"{int(num)} + ({int(num)} * 2) = {int(resultado)}")
